a1c30ee8bba7fe7c6e84b31415db3c4c30fb7b37
hypergraphman <r_aitov@mail.ru>
2017-10-07 21:05:33 +0300

    классная работа

---
+++ ClassWorks/Inheritance/src/ru/itpark/Main.java
@@ -0,0 +1,11 @@
+package ru.itpark;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        long x = 7;
+        int y = 'a' + 1;
+        System.out.println(y);
+    }
+}
